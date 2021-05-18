d1e42d3a37de8a77a29cb92824317091c19d128b
Harendra183k <83546250+Harendra183k@users.noreply.github.com>
2021-05-18 21:04:37 +0530

    Create Comment.c

---
+++ Comment.c
@@ -0,0 +1,9 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() 
+{
+    // Write C code here
+    printf("My name is Harendra kumawat I am from Jaipur");
+    /* The definition of above printf function is available in stdio.h header file*/
+        return 0;
+}
